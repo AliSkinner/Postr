9e16df174134b89b71df7c31aa88bc27f19da23a
Ali Skinner <awskinner26@gmail.com>
2015-05-15 14:44:45 +0100

    connect main controller

---
+++ app.js
@@ -0,0 +1,13 @@
+var app = angular.module('postr', []);
+
+app.controller('MainCtrl', [
+  '$scope',
+  function($scope){
+    $scope.test = 'Hello World';
+
+    $scope.posts = ['post 1',
+                    'post 2',
+                    'post 3',
+                    'post 4',
+                    'post 5']
+  }]);
