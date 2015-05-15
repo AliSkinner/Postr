34b26d575cd8a7a4b408ea5e6a5e1e92126c3885
Ali Skinner <awskinner26@gmail.com>
2015-05-15 14:59:43 +0100

    added upvote function

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ app.controller('MainCtrl', [
   {title: 'post 5', upvotes: 4}];
 
   $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') {return;}
     $scope.posts.push({title: $scope.title, upvotes:0});
     $scope.title = "";
   };
+
+  $scope.incrementUpvotes = function(post){
+    post.upvotes++;
+  };
+
   }]);
